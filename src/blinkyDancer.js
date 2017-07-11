84b4ebca0e251be6b6fbc3a0d4418a8681eaad86
Hack Reactor Student <student@hackreactor.com>
2017-07-10 20:44:48 -0700

    blink and dancer passing initla tests

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,10 +1,10 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
-  Dancer.apply(this, [top, left, timeBetweenSteps]);
-  
+  Dancer.apply(this, [top, left, timeBetweenSteps]); 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
-  var oldStep = Dancer.prototype.step;
+  this.oldStep = Dancer.prototype.step;
+  this.step();
+  
 
 
 
@@ -15,7 +15,8 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  oldStep();
+  
+  this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
--- src/dancer.js
+++ src/dancer.js
@@ -2,7 +2,7 @@
 var Dancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step();
+  this.timeBetween = timeBetweenSteps;
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
@@ -11,8 +11,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
 Dancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  var dancer = this;
-  setTimeout(dancer.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetween);
 };
 
 Dancer.prototype.setPosition = function(top, left) {
